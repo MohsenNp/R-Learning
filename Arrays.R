ad02cf6a52adc9cf019efce9a1adfeec2af09343
Future <mn7697np@gmail.com>
2017-11-09 20:16:27 +0330

    Working with Arrays

---
+++ Arrays.R
@@ -0,0 +1,8 @@
+###### Working with metrices ###### 
+# Least use DS in R
+# Similar to vector and matrix
+# can take on number of dimension (greater than 2)
+
+theArray <- array(1:12, dim = c(2, 3, 2)) # 3D Array
+theArray
+ 
