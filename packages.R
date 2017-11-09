c7d50d34c2f55182f216ad47074edbe636450fab
Future <mn7697np@gmail.com>
2017-11-09 16:40:56 +0330

    Adding packages functions in packages.R

--- packages.R
+++ packages.R
@@ -2,4 +2,13 @@
 install.packages(c("ggplot2", "coefplot")) # this is for installing several packages
 # The c() function is concatenation function (Use help(c) to see what does it do!)
 
+library(ggplot2) # load package in R (if "" is used, it'll work either)
+require(coefplot) # Does the same as library() function
+
+install.packages("useful")
+
+require(useful) # it'll include the dependant packages too! (useful --> ggplot2)
+
+
+
 
