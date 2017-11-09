075be97137bca157c9c8a91b2063f27fac3c9cc5
Future <mn7697np@gmail.com>
2017-11-09 16:28:31 +0330

    Adding packages.R

---
+++ packages.R
@@ -0,0 +1,5 @@
+###### Working With Packages ######
+install.packages(c("ggplot2", "coefplot")) # this is for installing several packages
+# The c() function is concatenation function (Use help(c) to see what does it do!)
+
+
